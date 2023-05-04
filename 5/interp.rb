d3776a9fa4837ddeb5b9a6016399be16fc7e586b
akmhmgc <akimuhamaguchi@icloud.com>
2023-05-04 14:02:03 +0900

    複数行の入力に対応

--- 5/interp.rb
+++ 5/interp.rb
@@ -1,14 +1,24 @@
 require 'minruby'
 
 def evaluate(tree)
-  return tree[1] if tree[0] == 'lit'
-
-  p evaluate(tree[2]) and return if tree[0] == 'func_call'
-
-  left = evaluate(tree[1])
-  right = evaluate(tree[2])
-  operator = tree[0]
-  eval("#{left} #{operator} #{right}")
+  case tree[0]
+  when 'lit'
+    tree[1]
+  when 'func_call'
+    p evaluate(tree[2])
+  when 'stmts'
+    last = nil
+    tree[1..-1].each do |statement|
+      last = evaluate(statement)
+      last
+    end
+    last
+  else
+    left = evaluate(tree[1])
+    right = evaluate(tree[2])
+    operator = tree[0]
+    eval("#{left} #{operator} #{right}")
+  end
 end
 
 str = minruby_load
