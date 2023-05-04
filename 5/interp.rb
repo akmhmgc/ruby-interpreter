dd1475712f62dc55499c16a44d6e7817ec5fd644
akmhmgc <akimuhamaguchi@icloud.com>
2023-05-04 15:54:35 +0900

    組み込み関数を利用可能にした

--- 5/interp.rb
+++ 5/interp.rb
@@ -1,32 +1,38 @@
 require 'minruby'
 
-def evaluate(tree, env = {})
+def evaluate(tree, lenv = {}, genv={ "p" => ["builtin", "p"]})
   case tree[0]
   when 'lit'
     tree[1]
   when 'func_call'
-    p evaluate(tree[2], env)
+    args = tree[2..-1].map { |arg| evaluate(arg, lenv, genv) }
+    mhd = genv[tree[1]]
+    case mhd[0]
+    when 'builtin'
+      minruby_call(mhd[1], args)
+    when 'user_defined'
+    end
   when 'stmts'
     last = nil
     tree[1..-1].each do |statement|
-      last = evaluate(statement, env)
+      last = evaluate(statement, lenv, genv)
       last
     end
     last
   when 'var_assign'
-    env[tree[1]] = evaluate(tree[2], env)
+    lenv[tree[1]] = evaluate(tree[2], lenv, genv)
   when 'var_ref'
-    env[tree[1]]
+    lenv[tree[1]]
   when 'if'
     # インタプリタの本質は、ターゲット言語の言語機能を、ホス言語の機能に丸投げすること
-    evaluate(tree[1], env) ? evaluate(tree[2], env) : evaluate(tree[3], env)
+    evaluate(tree[1], lenv, genv) ? evaluate(tree[2], lenv, genv) : evaluate(tree[3], lenv, genv)
   when 'while'
-    while evaluate(tree[1], env)
-      evaluate(tree[2], env)
+    while evaluate(tree[1], lenv, genv)
+      evaluate(tree[2], lenv, genv)
     end
   else
-    left = evaluate(tree[1], env)
-    right = evaluate(tree[2], env)
+    left = evaluate(tree[1], lenv, genv)
+    right = evaluate(tree[2], lenv, genv)
     operator = tree[0]
     eval("#{left} #{operator} #{right}")
   end
--- 5/test.rb
+++ 5/test.rb
@@ -1,5 +1,1 @@
-i = 0
-while i < 5
-  p i
-  i = i + 1
-end
+p(1,2,4)
