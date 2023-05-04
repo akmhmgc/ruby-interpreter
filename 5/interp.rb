4d3b9e405718561bf880efc558bd79d0fc37b7b3
akmhmgc <akimuhamaguchi@icloud.com>
2023-05-04 14:24:50 +0900

    変数の代入と変数の参照

--- 5/interp.rb
+++ 5/interp.rb
@@ -1,21 +1,25 @@
 require 'minruby'
 
-def evaluate(tree)
+def evaluate(tree, env = {})
   case tree[0]
   when 'lit'
     tree[1]
   when 'func_call'
-    p evaluate(tree[2])
+    p evaluate(tree[2], env)
   when 'stmts'
     last = nil
     tree[1..-1].each do |statement|
-      last = evaluate(statement)
+      last = evaluate(statement, env)
       last
     end
     last
+  when 'var_assign'
+    env[tree[1]] = evaluate(tree[2], env)
+  when 'var_ref'
+    env[tree[1]]
   else
-    left = evaluate(tree[1])
-    right = evaluate(tree[2])
+    left = evaluate(tree[1], env)
+    right = evaluate(tree[2], env)
     operator = tree[0]
     eval("#{left} #{operator} #{right}")
   end
